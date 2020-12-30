68413879a7fdf27f217cb2f957dcf3afe5e116b0
Tadeu Bernacchi <tadeu@MacBook.local>
2020-12-30 10:40:41 -0300

    Adjust install-jenkins.sh

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -6,3 +6,7 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 echo 'deb http://pkg.jenkins.io/debian-stable binary/' > /etc/apt/sources.list.d/jenkins.list
 apt update -y
 apt install jenkins -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+usermod -aG docker jenkins
+apt install -y python3-pip && pip3 install ansible && pip3 install openshift
