57fcef337acb049e26b3c524e24017b75f50c356
Tadeu Bernacchi <tadeu@Tadeus-MacBook-Pro.local>
2023-11-20 17:26:00 -0300

    fix-uploading-crisocket-unauthorized.sh

--- scripts/kubeadm-2023/fix-uploading-crisocket-unauthorized.sh
+++ scripts/kubeadm-2023/fix-uploading-crisocket-unauthorized.sh
@@ -1,4 +1,11 @@
 #!/bin/bash	
+# In case you received this kind of message trying to add a node join the cluster:
+# ...
+# [kubelet-start] Waiting for the kubelet to perform the TLS Bootstrap...
+# [kubelet-check] Initial timeout of 40s passed.
+# error execution phase kubelet-start: error uploading crisocket: Unauthorized
+# To see the stack trace of this error execute with --v=5 or higher
+# These steps fixed the issue on the worker node.
 swapoff -a # will turn off the swap
 kubeadm reset
 systemctl daemon-reload
